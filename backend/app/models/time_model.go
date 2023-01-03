577f8fda24b71a25df926a4171a47fc318ecad23
mintaq <minh.tranquang98@gmail.com>
2023-01-03 22:37:04 +0700

    Update

---
+++ backend/app/models/time_model.go
@@ -0,0 +1,8 @@
+package models
+
+import "time"
+
+type TimeModel struct {
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"` // Set to current time if it is zero on creating
+	UpdatedAt time.Time `json:"updated_at"`                       // Set to current unix seconds on updating or if it is zero on creating
+}
--- backend/app/models/user_model.go
+++ backend/app/models/user_model.go
@@ -1,16 +1,11 @@
 package models
 
-import (
-	"time"
-)
-
 // User struct to describe User object.
 type User struct {
-	ID           int       `json:"id" gorm:"primarykey"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
-	Email        string    `json:"email" validate:"required,email,lte=255"`
-	PasswordHash string    `json:"password_hash,omitempty" validate:"required,lte=255"`
-	UserStatus   int       `json:"user_status" validate:"required,len=1"`
-	UserRole     string    `json:"user_role" validate:"required,lte=25"`
+	ID           int    `json:"id" gorm:"primarykey"`
+	Email        string `json:"email" gorm:"unique" validate:"required,email,lte=255"`
+	PasswordHash string `json:"password_hash,omitempty" validate:"required,lte=255"`
+	UserStatus   int    `json:"user_status" validate:"required,len=1"`
+	UserRole     string `json:"user_role" validate:"required,lte=25"`
+	TimeModel
 }
