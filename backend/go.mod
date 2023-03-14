024ba63cdb83cc0d34bb316ad2ea436910f7d05f
mint4q <minh.tranquang98@gmail.com>
2023-03-14 17:08:52 +0700

    test socket.io

--- backend/go.mod
+++ backend/go.mod
@@ -33,7 +33,11 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/gofiber/websocket/v2 v2.1.4 // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/googollee/go-socket.io v1.7.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/lib/pq v1.10.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.10 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
--- backend/pkg/routes/public_routes.go
+++ backend/pkg/routes/public_routes.go
@@ -1,9 +1,15 @@
 package routes
 
 import (
+	"fmt"
+	"html"
+	"log"
 	"os"
 
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
+	socketio "github.com/googollee/go-socket.io"
 	"gitlab.xipat.com/omega-team3/qr-menu-backend/app/controllers"
 )
 
@@ -40,4 +46,52 @@ func PublicRoutes(a *fiber.App) {
 	v1.Post("/orders", controllers.CreateOrder)
 	v1.Post("/orders/checkout", controllers.CheckoutOrder)
 	v1.Post("/orders/pay", controllers.PayOrder)
+
+	// Routes for WS:
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+
+	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		return "recv " + msg
+	})
+
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("meet error:", e)
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		fmt.Println("closed", reason)
+	})
+
+	go server.Serve()
+	defer server.Close()
+
+	server1 := http.NewServeMux()
+	server1.Handle("/socket.io/", server)
+	server1.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("alo")
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	go func() {
+		log.Println("Server started on: http://localhost:9001")
+		log.Fatalln(http.ListenAndServe("localhost:9001", server1))
+	}()
 }
